d9c57014a534538e3b150c7b52ba0f20d356e5fa
Zomabies <zomabies@users.noreply.github.com>
2024-03-24 05:03:38 +0800

    Migrate to forgeconfigapiport v4 API

--- fabric/src/main/java/restorechatlinks/fabric/ConfigHelper.java
+++ fabric/src/main/java/restorechatlinks/fabric/ConfigHelper.java
@@ -1,7 +1,7 @@
 package restorechatlinks.fabric;
 
-import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
-import fuzs.forgeconfigapiport.api.config.v2.ModConfigEvents;
+import fuzs.forgeconfigapiport.fabric.api.forge.v4.ForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.api.forge.v4.ForgeModConfigEvents;
 import net.minecraftforge.fml.config.ModConfig;
 import restorechatlinks.RestoreChatLinks;
 import restorechatlinks.fabric.config.Config;
@@ -10,10 +10,10 @@ public class ConfigHelper {
 
     public static void RegisterConfig() {
         com.electronwill.nightconfig.core.Config.setInsertionOrderPreserved(true);
-        ModConfigEvents.loading(RestoreChatLinks.MOD_ID).register(ConfigHelper::onConfigLoad);
+        ForgeModConfigEvents.loading(RestoreChatLinks.MOD_ID).register(ConfigHelper::onConfigLoad);
         // "ModConfigEvents.loading" is register first since it immediately invoke "loading" after this statement
         ForgeConfigRegistry.INSTANCE.register(RestoreChatLinks.MOD_ID, ModConfig.Type.CLIENT, Config.clientSpec);
-        ModConfigEvents.reloading(RestoreChatLinks.MOD_ID).register(ConfigHelper::onConfigReload);
+        ForgeModConfigEvents.reloading(RestoreChatLinks.MOD_ID).register(ConfigHelper::onConfigReload);
     }
 
     private static void onConfigLoad(ModConfig config) {
